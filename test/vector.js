a6c7ae71408cbe9c2ad67f0af4c25e25ed9420ba
tommyZZM <452490446@qq.com>
2016-09-14 08:10:50 +0800

    feature: add Vextor.inLinerIndependen ... t

--- test/vector.js
+++ test/vector.js
@@ -7,13 +7,13 @@ const expect = require("chai").expect
 const Vector = require("../lib/Vector")
 
 describe("Vector",function () {
-    it("Vector.of(...)",function (done) {
+    it("Vector.of(number[])",function (done) {
         let v1 = Vector.of([1,1]);
         expect(/^1\.414/.test(v1.length)).to.be.equal(true);
         done()
     });
 
-    it("Vector.add(...)", function (done) {
+    it("Vector.add(a,b)", function (done) {
 
         let v1 = Vector.of([0,1]);
         let v2 = Vector.of([-1,-3]);
@@ -29,7 +29,7 @@ describe("Vector",function () {
         done()
     })
 
-    it("Vector.add(...) with error", function (done) {
+    it("Vector.add(a,b) with error", function (done) {
 
         let v1 = Vector.of([0,1]);
         let v2 = Vector.of([0,1,1]);
@@ -39,12 +39,24 @@ describe("Vector",function () {
         done()
     })
 
-    it("Vector.multiply(...)", function (done) {
+    it("Vector.multiply(times,vector)", function (done) {
 
         let v1 = Vector.of([1,2]);
         let v2 = Vector.multiply(3,v1);
 
         expect(Vector.equals(v2,Vector.of([3,6]))).to.be.equal(true);
+
+        done()
+    })
+
+    it("Vector.isLinerIndependent(Vector[])", function (done) {
+        let v1 = Vector.of([1,2]);
+        let v2 = Vector.multiply(3,v1);
+        let v3 = Vector.multiply(-1,v2);
+        let v4 = Vector.of([1,1])
+
+        expect(Vector.isLinerIndependent([v1,v2,v3])).to.be.equal(false);
+        expect(Vector.isLinerIndependent([v1,v4])).to.be.equal(true);
 
         done()
     })
